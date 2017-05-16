6ec9e3eb74701d4c34744473c799241809402db9
Joseph Borromeo <joseph.borromeo00@gmail.com>
2017-05-16 12:14:53 -0400

    Initial Commit

---
+++ Rooms.py
@@ -0,0 +1,46 @@
+import time
+room_list = []
+room = ["Center Starting Room",None,6, 1, 14 ]
+room_list.append(room)
+room = ["Hallway 1", 0, None, 2, None]
+room_list.append(room)
+room = ["Cell Hall 1", 1, 21, 3, None]
+room_list.append(room)
+room = ["Cell Hall 2", 2, 4, None, 5]
+room_list.append(room)
+room = ["Men's Bathroom", None, None, None, 3]
+room_list.append(room)
+room = ["Women's Bathroom", None, 3, None, None]
+room_list.append(room)
+room = ["Long Corridor Part 1", None, 7, 22, 0]
+room_list.append(room)
+room = ["Long Corridor Part 2", 12, 8, None, 6]
+room_list.append(room)
+room = ["Long Corridor Part 3", None, 9, None, 7]
+room_list.append(room)
+room = ["Storage Room", 10, None, 11, None]
+room_list.append(room)
+room = ["Electrical Room", None, None, 9, None]
+room_list.append(room)
+room = ["Weaponry", 9, None, None, 21]
+room_list.append(room)
+room = ["Medical Room", None, None, 7, 13]
+room_list.append(room)
+room = ["Employee Room", None, 12, None, None]
+room_list.append(room)
+room = ["Library", 17, 0, 15, None]
+room_list.append(room)
+room = ["Secret Library Corridor", 14, None, 16, None]
+room_list.append(room)
+room = ["Secret Lab", 15, None, None, None]
+room_list.append(room)
+room = ["Outside Courtyard", 18, None, 14, None]
+room_list.append(room)
+room = ["Security/ Surveillance Office", None, 19, 17, None]
+room_list.append(room)
+room = ["Head Office", None, 20, None, 18]
+room_list.append(room)
+room = ["Escape", None, 20, None, 19]
+room_list.append(room)
+room = ["Kitchen", 6, None, None, None]
+room_list.append(room)
