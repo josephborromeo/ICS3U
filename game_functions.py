36e5d5b31ad749ce3179967648fe271505c2a857
Joseph Borromeo <joseph.borromeo00@gmail.com>
2017-05-17 13:13:49 -0400

    Added commands and new command function

--- game_functions.py
+++ game_functions.py
@@ -1,4 +1,22 @@
+import time
 from Rooms import *
+
+def commands(input):
+    input = input.split(" ")
+    if input[0] == "/help":
+        print("\t\tCommands\n---------------------------------------\n/help\tPrints help dialog\n/search\tSearches the room for entities")
+
+    elif input[0] == "/search":
+        print("Searching Room", end = '')
+        time.sleep(0.25)
+        print ('.', end = '')
+        time.sleep(0.25)
+        print('.', end ='')
+        time.sleep(0.25)
+        print('.', end ='')
+
+    else:
+        print("That is not a valid input, type /help for a list of commands")
 
 def input_parser(input):
     global current_room, room_list
@@ -35,4 +53,4 @@ def input_parser(input):
             print(current_room)
             print(room_list[current_room][0])
     else:
-        print("That is not a valid input")
+        commands(input)
--- main.py
+++ main.py
@@ -8,6 +8,6 @@ done = False
 print(room_list[current_room][0])
 
 while not done:
-    player_input = input("What player_input would you like to go in - N, S, E, W? ")
+    player_input = input("\nWhat direction would you like to go in - N, S, E, W? ")
     player_input = player_input.lower()
     input_parser(player_input)
